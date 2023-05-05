e8a2ff7041c24b37b598339310e52fa5e87b4e7b
Rabotyaga1155 <123551706+Rabotyaga1155@users.noreply.github.com>
2023-05-05 11:31:54 +0500

    Add files via upload

---
+++ prakt16_3/prakt16_3/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace prakt16_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            double[] arr = { 5.1, 1.3, 9.2, 2.3,5.1,3};
+            double[] uniqueNums = getnumbers(arr);
+            int[] f = new int[uniqueNums.Length];
+            for (int i =0;i<uniqueNums.Length;i++)
+            {
+                f[i] = getf(arr, uniqueNums[i]);
+                Console.WriteLine($"{uniqueNums[i]} - {f[i]}");
+            }
+            double[] newArr = new double[arr.Length];
+            for(int i = 0;i<arr.Length;i++)
+            {
+                double num = arr[i];
+                int index = Array.IndexOf(uniqueNums, num);
+                newArr[i] = num * f[index];
+            }
+            Console.WriteLine("Новый массив: ");
+                foreach(var i in newArr)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+            static int getf(double[] arr,double num)
+            {
+                int f = 0;
+                foreach (double n in arr)
+                {
+                    if (n == num)
+                    {
+                        f++;
+                    }
+                }
+                return f;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            static double[] getnumbers(double[] arr)
+            {
+                double[] uniqueNum = new double[arr.Length];
+                int i = 0;
+                foreach (double a in arr)
+                {
+                    if (Array.IndexOf(uniqueNum,a)==-1)
+                    {
+                        uniqueNum[i] = a;
+                        i++;
+                    }
+                }
+                Array.Resize(ref uniqueNum, i);
+                return uniqueNum;
+            }
+        }
+    }
+}
---
+++ prakt16_4/prakt16_4/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prakt16_4
+{
+    public partial class Form1 : Form
+    {
+        
+        string path = "Strani.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            long chislen = long.Parse(textBox1.Text);
+            Dictionary<string, long> countries = new Dictionary<string, long>();
+            string[] lines = File.ReadAllLines(path);
+            foreach(string line in lines)
+            {
+                string[] parts = line.Split(' ');
+                string country = parts[0];
+                if (long.TryParse(parts[1],out long pop)&& pop>chislen)
+                {
+                    countries.Add(country, pop);
+                }
+
+
+
+                //long pop = long.Parse(parts[1]);
+                //if (pop>chislen)
+                //{
+                //    countries.Add(country, pop);
+                //}
+            }
+            var sorted = countries.OrderBy(x => x.Key.Length).ThenBy(x => x.Key);
+            listBox1.Items.Clear();
+            foreach (var country in sorted)
+            {
+                listBox1.Items.Add($"{country.Key} ({country.Value})");
+            }
+        }
+    }
+}
